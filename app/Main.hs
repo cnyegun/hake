abd6ff526a87457fa27a1e8a4eb1b25164888228
Chi Nguyen <buichinguyen1105@gmail.com>
2026-05-06 20:55:06 +0300

    reformat

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,19 @@
-import System.Process ( callCommand )
-import System.Directory ( doesFileExist, getModificationTime )
-import Control.Monad ( unless )
-import System.Exit ( exitFailure, exitSuccess )
+import Control.Monad (unless)
 import qualified Data.Map.Strict as Map
+import System.Directory (doesFileExist, getModificationTime)
+import System.Exit (exitFailure, exitSuccess)
+import System.Process (callCommand)
 
 type Target = FilePath
+
 type Dep = FilePath
+
 type Action = String
 
 data Rule = Rule
-  { target :: Target
-  , deps   :: [Dep]
-  , action :: Action
+  { target :: Target,
+    deps :: [Dep],
+    action :: Action
   }
 
 type BuildSpec = Map.Map Target Rule
@@ -22,48 +24,51 @@ checkFreshTarget target deps = do
   depTimes <- traverse getModificationTime deps
   pure $ all (<= targetTime) depTimes
 
-ruleOut :: Rule 
-ruleOut = Rule
-          { target = "test.out"
-          , deps = ["test.o"]
-          , action = "gcc test.o -o test.out"
-          }
+ruleOut :: Rule
+ruleOut =
+  Rule
+    { target = "test.out",
+      deps = ["test.o"],
+      action = "gcc test.o -o test.out"
+    }
 
 ruleObj :: Rule
-ruleObj = Rule
-          { target = "test.o"
-          , deps = ["test.c"]
-          , action = "gcc -c test.c -o test.o"
-          }
+ruleObj =
+  Rule
+    { target = "test.o",
+      deps = ["test.c"],
+      action = "gcc -c test.c -o test.o"
+    }
 
 spec :: BuildSpec
-spec = Map.fromList
-      [ ("test.out", ruleOut)
-      , ("test.o", ruleObj)
-      ]
+spec =
+  Map.fromList
+    [ ("test.out", ruleOut),
+      ("test.o", ruleObj)
+    ]
 
 getRule :: Target -> BuildSpec -> Maybe Rule
 getRule = Map.lookup
 
 build :: Target -> BuildSpec -> IO ()
 build t sp = do
-  case getRule t sp of 
-    Just rule -> do 
-                mapM_ (`build` sp) (deps rule)
-                targetExists <- doesFileExist t
-                isFresh <- if targetExists then checkFreshTarget t (deps rule)
-                            else pure False
+  case getRule t sp of
+    Just rule -> do
+      mapM_ (`build` sp) (deps rule)
+      targetExists <- doesFileExist t
+      isFresh <-
+        if targetExists
+          then checkFreshTarget t (deps rule)
+          else pure False
 
-                unless isFresh $ do
-                  putStrLn (action rule) 
-                  callCommand (action rule)
-
-
-    Nothing   -> do 
-                fileExists <- doesFileExist t
-                unless fileExists $ do
-                  putStrLn ("No rule found for " ++ t ++ ".")
-                  exitFailure
+      unless isFresh $ do
+        putStrLn (action rule)
+        callCommand (action rule)
+    Nothing -> do
+      fileExists <- doesFileExist t
+      unless fileExists $ do
+        putStrLn ("No rule found for " ++ t ++ ".")
+        exitFailure
 
 main :: IO ()
 main = do
